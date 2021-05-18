29edb7fe057d3dcfcbf0e016d683169eb792499e
A284Philipi <78768392+A284Philipi@users.noreply.github.com>
2021-05-18 20:20:57 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char a = 40, b = 41, c = 34, d = 95, e = 92, f = 47;
+    cout << c << "Ro'b'er\tto" << e << f << c <<endl;
+    cout << "(._.) ( l: ) ( .-. ) ( :l ) (._.)" <<endl;
+    cout << "(^_-) (-_-) (-_^)" <<endl;
+    cout << a << c << d << c << b << " ('.')" <<endl;
+    return 0;
+}
